e2c30d36bb1515d36207ec2c7874320f7a63692b
SVickyPawar <97332040+SVickyPawar@users.noreply.github.com>
2022-04-07 01:06:42 +0530

    webpack assignment

---
+++ webPack/build/bundle.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ (() => {
+
+eval("var h1=document.createElement(\"h1\");\r\nh1.innerText=\"Notes\";\r\n\r\nh1.setAttribute(\"class\",\"col\");\r\n\r\ndocument.querySelector(\"body\").append(h1)\n\n//# sourceURL=webpack://we1/./src/index.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	
+/******/ })()
+;
---
+++ webPack/src/index.js
@@ -0,0 +1,6 @@
+var h1=document.createElement("h1");
+h1.innerText="Notes";
+
+h1.setAttribute("class","col");
+
+document.querySelector("body").append(h1)
